a63572bff1f3389b3ec4c62ac019d0f4600e3acc
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-07-17 17:45:00 +0530

    Add project foundation and configuration

--- config/app.php
+++ config/app.php
@@ -6,4 +6,5 @@ return [
     'timezone' => $_ENV['APP_TIMEZONE'] ?? 'Asia/Colombo',
     'delivery_fee' => (int) ($_ENV['APP_DELIVERY_FEE'] ?? 500),
     'pagination' => 12,
+    'ssl_ca_file' => trim($_ENV['SSL_CA_FILE'] ?? ''),
 ];
