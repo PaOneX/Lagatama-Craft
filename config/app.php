253ca7ea13a99940bf35428ca6899dfca690f558
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-08-02 10:30:00 +0530

    Add project foundation and configuration

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => $_ENV['APP_NAME'] ?? 'Lagatama Craft',
+    'url' => rtrim($_ENV['APP_URL'] ?? 'http://localhost', '/'),
+    'timezone' => $_ENV['APP_TIMEZONE'] ?? 'Asia/Colombo',
+    'delivery_fee' => (int) ($_ENV['APP_DELIVERY_FEE'] ?? 500),
+    'pagination' => 12,
+];
