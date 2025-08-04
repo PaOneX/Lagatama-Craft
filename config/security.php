4617490d081d9a560ff702263f1515c82037ea7c
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-08-04 17:45:00 +0530

    Add project foundation and configuration

---
+++ config/security.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'session_idle_minutes' => (int) ($_ENV['SESSION_IDLE_MINUTES'] ?? 30),
+    'login_max_attempts' => (int) ($_ENV['LOGIN_MAX_ATTEMPTS'] ?? 5),
+    'login_lockout_minutes' => (int) ($_ENV['LOGIN_LOCKOUT_MINUTES'] ?? 15),
+    'upload_max_image_bytes' => (int) ($_ENV['UPLOAD_MAX_IMAGE_BYTES'] ?? 5 * 1024 * 1024),
+    'upload_max_video_bytes' => (int) ($_ENV['UPLOAD_MAX_VIDEO_BYTES'] ?? 50 * 1024 * 1024),
+];
+
