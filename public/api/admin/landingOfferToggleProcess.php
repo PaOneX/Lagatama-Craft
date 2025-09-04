e64185e9b3f54f2fe39e7ecb09f7e85c9991afd8
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-09-04 10:30:00 +0530

    Wire admin management API endpoints

---
+++ public/api/admin/landingOfferToggleProcess.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once dirname(__DIR__, 2) . '/init.php';
+
+use App\Core\Auth;
+use App\Core\Response;
+use App\Services\LandingService;
+
+Auth::requireAdmin();
+Auth::verifyCsrfOrFail();
+
+$id = (int) ($_POST['id'] ?? 0);
+Response::text((new LandingService())->toggleOffer($id));
