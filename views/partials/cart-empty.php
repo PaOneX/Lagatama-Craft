13064028bbb12e6145fcea71ea2c2c4beefe1a98
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-09-22 10:30:00 +0530

    Add view templates and layouts

---
+++ views/partials/cart-empty.php
@@ -0,0 +1,6 @@
+<div class="lc-empty">
+    <i class="bi bi-cart-x"></i>
+    <h2>Your cart is empty</h2>
+    <p>Browse our handcrafted collection and add something you love.</p>
+    <a href="home.php" class="lc-btn lc-btn-primary mt-3">Start Shopping</a>
+</div>
